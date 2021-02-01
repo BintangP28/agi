467b899db8e8e882ae28e24b0b27f33000e38618
BintangP28 <77977048+BintangP28@users.noreply.github.com>
2021-02-01 16:44:02 +0700

    Add files via upload

---
+++ plugins/info.js
@@ -0,0 +1,39 @@
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+  conn.reply(m.chat, `
+┣━⊱ ❰ *INFO BOT* ❱ ═
+┣⊱ _Dibuat dengan javascript via NodeJs_
+┣⊱ _Rec: Bintang Pamungkas_
+┣⊱ _Script: @Nurotomo_
+┃          
+┣━⊱ ❰ *Thanks To* ❱ ═
+┣⊱ _Kelvin:v_
+┣⊱ _Izbullah:v_
+┣⊱ _Me:v_
+┣⊱ _Dan kawan yang lain :)_
+┃
+┣━⊱❰ *DONASI* ❱ ═
+┣⊱ GOPAY: 0838-1330-4760
+┣⊱ DANA: 0838-1330-4760
+┣⊱ XL/AXIS:0859-2992-2766 / 0838-1330-4760
+┃
+┣━⊱ Request? Wa.me/6283813304760
+┃
+  ╰══─⊱  ❰ *Bintang Pamungkas* ❱ ⊰─══╯
+`.trim(), m)
+}
+handler.help = ['info']
+handler.tags = ['info']
+handler.command = /^(info)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
+
